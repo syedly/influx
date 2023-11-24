e244cb553cab3903c1965caac5ae48d42b8c35b0
syedly <hassanshah9152@gmail.com>
2023-11-24 06:04:53 -0800

    deployment changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s#+h3#cw6lx#eg$tvxcome4%f_hev!!=@@c_*$+$i**y*@2bke'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
